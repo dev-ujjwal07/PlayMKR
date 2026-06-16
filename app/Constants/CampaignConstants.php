36bc22e329c88dff1f6c3687ac5ae3914ec1e017
ujjwal <bhattujjwal88@gmail.com>
2026-06-16 19:39:51 +0530

    added create new deal

---
+++ app/Constants/CampaignConstants.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Constants;
+
+class CampaignConstants
+{
+    const CAMPAIGN_CREATED =
+        'Campaign created successfully';
+
+    const DEAL_TYPE_NOT_FOUND =
+        'Deal type not found';
+
+    const DELIVER_TYPE_NOT_FOUND =
+        'Deliver type not found';
+
+    const SPONSOR_NOT_FOUND =
+        'Sponsor not found';
+}
